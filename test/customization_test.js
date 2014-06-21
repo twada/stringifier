e78e35a47670e50cddc82ced999f5c8e555977b1
Takuto Wada <takuto.wada@gmail.com>
2014-06-22 00:20:26 +0900

    reorganize strategies tests

--- test/customization_test.js
+++ test/customization_test.js
@@ -1,9 +1,10 @@
 var stringify = require('..'),
     filters = stringify.filters,
     f = stringify.filters.f,
+    s = stringify.filters.s,
     assert = require('assert');
 
-describe('filters', function () {
+describe('strategies', function () {
     function Student (name, age, gender) {
         this.name = name;
         this.age = age;
@@ -20,55 +21,44 @@ describe('filters', function () {
 
     it('fixed', function () {
         var handlers = {
-            'Student': filters.skipChildren(filters.fixed('BOOM'))
+            'Student': s.fixed('BOOM')
         };
         assert.equal(stringify(this.student, null, handlers), 'BOOM');
     });
 
     it('prune', function () {
         var handlers = {
-            'Student': filters.prune()
+            'Student': s.prune()
         };
         assert.equal(stringify(this.student, null, handlers), '#Student#');
     });
 
     it('json', function () {
         var handlers = {
-            'Student': filters.json()
+            'Student': s.json()
         };
         assert.equal(stringify(this.student, null, handlers), '{"name":"tom","age":10,"gender":"M"}');
     });
 
     it('toStr', function () {
         var handlers = {
-            'Student': filters.toStr()
+            'Student': s.toStr()
         };
         assert.equal(stringify(this.student, null, handlers), '[object Object]');
     });
     
     it('newLike', function () {
         var handlers = {
-            'Student': filters.newLike()
+            'Student': s.newLike()
         };
         assert.equal(stringify(this.student, null, handlers), 'new Student({"name":"tom","age":10,"gender":"M"})');
     });
 
-    it('composite newLike filter', function () {
-        var handlers = {
-            'Student': filters.newLike(filters.fixed('WOA'))
-        };
-        assert.equal(stringify(this.student, null, handlers), 'new Student(WOA)');
-    });
-    
     it('number', function () {
-        assert.equal(stringify([NaN, 0, Infinity, -0, -Infinity]), '[NaN,0,Infinity,0,-Infinity]');
-    });
-
-    it('composite number filter', function () {
         var handlers = {
-            'number': filters.number(filters.fixed('BOO'))
+            'number': s.number()
         };
-        assert.equal(stringify([NaN, 0, Infinity, -0, -Infinity], null, handlers), '[NaN,BOO,Infinity,BOO,-Infinity]');
+        assert.equal(stringify([NaN, 0, Infinity, -0, -Infinity], null, handlers), '[NaN,0,Infinity,0,-Infinity]');
     });
 });
 
