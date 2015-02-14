5cc4f759ef008a305d00574a87e86b22282f8120
Takuto Wada <takuto.wada@gmail.com>
2015-02-14 14:59:58 +0900

    test(stringifier): add bug reproduction testcase
    For example, on PhantomJS, iterating HTMLInputElement causes TypeError on accessing some (maybe hidden) properties such as `selectionEnd`

---
+++ test/runtime_type_error_test.js
@@ -0,0 +1,27 @@
+(function (root, factory) {
+    'use strict';
+    if (typeof define === 'function' && define.amd) {
+        define(['stringifier', 'assert'], factory);
+    } else if (typeof exports === 'object') {
+        factory(require('..'), require('assert'));
+    } else {
+        factory(root.stringifier, root.assert);
+    }
+}(this, function (
+    stringifier,
+    assert
+) {
+
+var stringify = stringifier.stringify;
+
+if (typeof document !== 'undefined' && typeof document.getElementById === 'function') {
+    describe('in case of runtime TypeError', function () {
+        it('stringifying HTMLInputElement', function () {
+            var input = document.getElementById("confirmation");
+            var str = stringify(input, {maxDepth: 1});
+            assert.equal(str, '');
+        });
+    });
+}
+
+}));
