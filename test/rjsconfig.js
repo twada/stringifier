0beae25b0fa72d4cdd0acc89a817c467454123e1
Takuto Wada <takuto.wada@gmail.com>
2015-07-10 16:55:05 +0900

    chore(test): should not depend on es5-shim

--- test/rjsconfig.js
+++ test/rjsconfig.js
@@ -2,7 +2,6 @@ var require = {
     paths: {
         stringifier: "../build/stringifier",
         "type-name": "../bower_components/type-name/build/type-name",
-        "es5-shim": "../bower_components/es5-shim/es5-shim",
         assert: "../bower_components/assert/assert",
         mocha: "../bower_components/mocha/mocha",
         requirejs: "../bower_components/requirejs/require"
