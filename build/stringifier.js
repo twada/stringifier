1d9a9b6c09de5eecadaa75e3282f668c71003e2d
Takuto Wada <takuto.wada@gmail.com>
2015-07-10 16:58:32 +0900

    chore(bundle): rebuild web bundle

--- build/stringifier.js
+++ build/stringifier.js
@@ -157,7 +157,7 @@ module.exports = stringifier;
  */
 
 module.exports = function (arr, fn, self) {
-  if (arr.filter) return arr.filter(fn);
+  if (arr.filter) return arr.filter(fn, self);
   if (void 0 === arr || null === arr) throw new TypeError;
   if ('function' != typeof fn) throw new TypeError;
   var ret = [];
